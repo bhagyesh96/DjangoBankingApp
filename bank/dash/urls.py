58cebb0bc10bfb6565d46a115bc2e3f438fd3cc4
Bhagyesh Panchal <bhagyeshpanchal1996@gmail.com>
2018-02-22 19:57:18 +0530

    Django 2 illustration in url is added
    how url are changed in Django2 illustrated

--- bank/dash/urls.py
+++ bank/dash/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url,include
+from django.urls import include, path, re_path  #for django 2
 from . import views
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
@@ -8,12 +9,12 @@ urlpatterns = [
     #dash
     url(r'^$',views.IndexView.as_view(),name="index"),
     #dash/2
-    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name="detail"),
+    path('<int:pk>/',views.DetailView.as_view(),name="detail"),
     #dash/sample,
     url(r'^sample', TemplateView.as_view(template_name="base.html"), name='sample'),
     url(r'^logout/$', auth_views.logout,{'next_page': '/login'}, name='logout'), #benifit of using next is it mooves out of app with help of url
     url(r'^search/?$', views.search, name='search'), # ? shows that url contain query
     #url(r'^sucess/(?P<user_id>[0-9]+)/?$', views.addBalance, name='sucess'),
-    url(r'^(?P<pk>[0-9]+)/update/$', views.update.as_view(), name='update'),
+    re_path(r'^(?P<pk>[0-9]+)/update/$', views.update.as_view(), name='update'), ## its Django 2 syntax
     url(r'^(?P<pk>[0-9]+)/addmoney/?$', views.addBalance, name='addmoney'),
 ]
